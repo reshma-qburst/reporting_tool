17f3792e9fd349112e32b5fce81089183cddbfea
reshma <reshma@qburst.com>
2016-08-01 17:27:12 +0530

    reading drop down values from json file

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,6 +1,15 @@
 (function(){
 'use strict';
-	app.controller('homeController',['$scope',function($scope){
-		
+	app.controller('homeController',['$scope','loadJson',function($scope,loadJson){
+		loadJson.getTripList().then(function(tripListData){
+			$scope.tripList = [];
+			$scope.driverList = [];
+			angular.forEach(tripListData.data, function(item){
+				angular.forEach(item.trip, function(tripItem){
+					$scope.tripList.push(tripItem);
+				});
+				$scope.driverList.push(item);
+			});
+		});
 	}]);
 })();
