ad84d569f26e2a403fd6e77b00d786e3f093440b
reshma <reshma@qburst.com>
2016-08-03 17:44:45 +0530

    rating directive

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,6 +1,8 @@
 (function(){
 'use strict';
-	app.controller('homeController',['$scope','loadJson',function($scope,loadJson){
+	app.controller('homeController',['$scope','loadJson',
+		function($scope,loadJson){
+
 		loadJson.getTripList().then(function(tripListData){
 			$scope.tripList = [];
 			$scope.driverList = [];
@@ -11,5 +13,30 @@
 				$scope.driverList.push(item);
 			});
 		});
+
+		$scope.rating = 0;
+	    $scope.ratings = [{
+	        current: 1,
+	        max: 5
+	    }, {
+	        current: 1,
+	        max: 5
+	    }, {
+	        current: 1,
+	        max: 5
+	    }];
+
+	    $scope.getSelectedRating = function (rating) {
+	    }
+
+	    $scope.saveRating = function(){		
+			if($scope.driverRatingForm.$valid) {
+				console.log("saved!");
+			}
+			else{
+				console.log("invalid");
+			}
+		};
+
 	}]);
 })();
---
+++ app/services/saveRating.js
@@ -0,0 +1,14 @@
+(function(){
+ 'use strict';
+	app.service('saveRating',function(){
+		var ratingArray = [];
+		return{
+			saveActivity : function(){
+				ratingArray.push({
+
+				});
+				return ratingArray;
+			}
+		}
+	});
+})();
