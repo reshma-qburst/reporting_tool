5ff6b7df256e64f991279fcc813de5adc85fab1a
reshma <reshma@qburst.com>
2016-08-12 17:04:39 +0530

    added filter and stacked bar chart changes

--- app/directives/normalVBarDirective.js
+++ app/directives/normalVBarDirective.js
@@ -1,29 +1,32 @@
-app.directive('barChart', function() {
-    var chart = d3.custom.barChart();
-    return {
-        restrict: 'E',
-        replace: true,
-        template: '<div class="chart"></div>',
-        scope: {
-            height: '=height',
-            data: '=data',
-            hovered: '&hovered'
-        },
-        link: function(scope, element, attrs) {
+(function() {
+    'use-strict';
+    app.directive('barChart', function() {
+        var chart = d3.custom.barChart();
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="chart"></div>',
+            scope: {
+                height: '=height',
+                data: '=data',
+                hovered: '&hovered'
+            },
+            link: function(scope, element, attrs) {
 
-            var chartEl = d3.select(element[0]);
+                var chartEl = d3.select(element[0]);
 
-            chart.on('customHover', function(d, i) {
-                scope.hovered({ args: d });
-            });
+                chart.on('customHover', function(d, i) {
+                    scope.hovered({ args: d });
+                });
 
-            scope.$watch('data', function(newVal, oldVal) {
-                chartEl.datum(newVal).call(chart);
-            });
+                scope.$watch('data', function(newVal, oldVal) {
+                    chartEl.datum(newVal).call(chart);
+                });
 
-            scope.$watch('height', function(d, i) {
-                chartEl.call(chart.height(scope.height));
-            })
+                scope.$watch('height', function(d, i) {
+                    chartEl.call(chart.height(scope.height));
+                })
+            }
         }
-    }
-});
+    });
+})();
